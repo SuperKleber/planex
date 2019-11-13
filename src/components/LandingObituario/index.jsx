807f4554435fbe17a3b8ebff10566549a6923c3c
Kevin Kleber Rivamontan Alvarado <alejandrofaesta@gmail.com>
2019-11-12 21:38:50 -0400

    🕷  import Card Persond

--- src/components/LandingObituario/index.jsx
+++ src/components/LandingObituario/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Grid, Container } from "@material-ui/core";
-import CardPerson from "./cardPerson";
+import CardPerson from "./CardPerson";
 import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
   item: {}
