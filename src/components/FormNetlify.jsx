27ffb328586e6aff8a863a0737becbcb999b75ac
Kevin Kleber Rivamontan Alvarado <alejandrofaesta@gmail.com>
2019-12-12 13:44:31 -0400

    Form

--- src/components/FormNetlify.jsx
+++ src/components/FormNetlify.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const FormNetlify = () => {
   return (
     <>
-      <form method="POST" data-netlify="true" netlify="true" action="/" hidden>
+      <form
+        name="contacto"
+        method="POST"
+        data-netlify="true"
+        netlify="true"
+        action="/"
+        hidden
+      >
         <input type="hidden" name="form-name" value="contacto" hidden />
         <input type="text" name="nombres" />
         <input type="text" name="apellidos" />
@@ -11,7 +18,14 @@ const FormNetlify = () => {
         <input type="email" name="email" />
         <input type="text" name="mensaje" />
       </form>
-      <form method="POST" data-netlify="true" netlify="true" action="/" hidden>
+      <form
+        name="prevision"
+        method="POST"
+        data-netlify="true"
+        netlify="true"
+        action="/"
+        hidden
+      >
         <input type="hidden" name="form-name" value="prevision" hidden />
         <input type="text" name="nombres" />
         <input type="text" name="apellidos" />
